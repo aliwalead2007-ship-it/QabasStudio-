03e54d68751eff1b5558de4266f5d7dc9df348ed
aliwalead2007-ship-it <aliwalead.2007@gmail.com>
2026-09-17 19:59:20 +0000

    إصلاح CI: إضافة JitPack repository + COVERR_API_KEY في الإعدادات

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
     }
     mavenCentral()
     gradlePluginPortal()
+    maven { url "https://www.jitpack.io" }
   }
 }
 
@@ -19,6 +20,7 @@ dependencyResolutionManagement {
   repositories {
     google()
     mavenCentral()
+    maven { url "https://www.jitpack.io" }
   }
 }
 
